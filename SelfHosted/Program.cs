ecf2a11e43945a61ec8504a9ee9bb3f9b63f6ac3
ncltrdorsor <serrano.nicolas.jose@gmail.com>
2017-11-12 21:18:24 +0100

    Add project files.

---
+++ SelfHosted/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfHosted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
